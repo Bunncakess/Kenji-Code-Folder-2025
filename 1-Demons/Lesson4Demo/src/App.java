d9393d081c45b7503335bfb9b0212ae8f8b6f036
Bunncakess <159061300+Bunncakess@users.noreply.github.com>
2024-09-16 08:50:07 -0600

    Done

---
+++ 1-Demons/Lesson4Demo/src/App.java
@@ -0,0 +1,10 @@
+//Project: Lesson 4 Demo\
+//Name: Kenji
+//Date: 13 Sept 2024
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ ASCII Animal/src/App.java
@@ -0,0 +1,11 @@
+//ASSIC
+//Kenji Obena
+//September 16 2024'
+//Draw a creature/animal using ASCII art.
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("./\\,,,,,,,/\\. \n(=O wwwww O=) \n( U       U ) \n(           )~~* \n   o     o");
+        System.out.println("\n Hello!");
+    }
+}
