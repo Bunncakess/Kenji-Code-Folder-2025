26d2229379c7b757c48638c13b1408b3a4b1d3aa
Bunncakess <159061300+Bunncakess@users.noreply.github.com>
2024-09-23 09:44:49 -0600

    YGES

---
+++ Assignment 5 Passing Methods 2024/src/Homer.java
@@ -0,0 +1,13 @@
+// Kenji
+// Mr Fox
+// 9/23/24
+// Passing Methods
+
+public class Homer {
+        public void FIRSTNAME(){
+            System.out.println("I AM HOMER");
+        }
+        public void LASTNAME(){
+            System.out.println("SIMPSON!!!!!");
+        }
+}
---
+++ Assignment 5 Passing Methods 2024/src/Simpson.java
@@ -0,0 +1,13 @@
+//Kenji
+// Mr Fox
+// 9/23/24
+// Passing Methods
+
+public class Simpson {
+    public static void main(String[] args) throws Exception {
+        Homer name = new Homer();
+
+        name.FIRSTNAME();
+        name.LASTNAME();
+    }
+}
