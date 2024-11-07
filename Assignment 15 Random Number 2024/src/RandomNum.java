290716dbbc59e383dbd04c91a90cbcdffdd1f887
Bunncakess <159061300+Bunncakess@users.noreply.github.com>
2024-11-07 09:27:31 -0600

    asda
    asda

--- Assignment 15 Random Number 2024/src/RandomNum.java
+++ Assignment 15 Random Number 2024/src/RandomNum.java
@@ -4,16 +4,17 @@ public class RandomNum {
     public static void main(String[] args) throws Exception {
         Random ranbo = new Random();
         Sample test = new Sample();
+        
+        int count = 1;
 
-        for (int i = 0; i < 11; i++ ){
+        for (int i = 1; i < 11; i++ ){
             int a  = ranbo.nextInt(1000);
             int b  = ranbo.nextInt(1000);
             
     
-            System.out.println("Here are you two random numbers:\n" + a + " and " + b + "\n");
+            System.out.println("#" + count + " Here are you two random numbers:\n" + a + " and " + b + "\n");
             test.check(a, b);
+            count++;
         }
-
-
     }
 }
